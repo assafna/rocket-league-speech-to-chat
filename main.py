b4f32dc57d92ccbe9e8068ab392ce5e6095393b4
assaf <nahum.assaf@gmail.com>
2021-11-13 20:15:55 +0200

    added pauses to not miss words

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def speech_to_chat(_pre):
             _audio_data = _r.listen(_source)
             _text = _r.recognize_google(_audio_data)
         print(_text)
-        pyautogui.write(_pre + _text)
+        keyboard.press(_pre)
+        pyautogui.sleep(0.1)
+        pyautogui.write(_text)
+        pyautogui.sleep(0.1)
         keyboard.press('enter')
     except Exception:
         pass
